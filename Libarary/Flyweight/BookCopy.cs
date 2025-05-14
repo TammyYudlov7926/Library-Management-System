7cc430d58eaa82a3799eba85a4f3c5e055309047
tammyyudlov <t0583287926@gmail.com>
2025-05-14 23:23:06 +0300

    Factory

--- Libarary/Flyweight/BookCopy.cs
+++ Libarary/Flyweight/BookCopy.cs
@@ -9,7 +9,27 @@ namespace Libarary.Flyweight
     internal class BookCopy
     {
 
-        
+        public int copies { get; set; }
+        public string Name { get; }
+        public string Author { get; }
+        public BookCategory Category { get; }
+
+        public BookCopy(string name, string author, BookCategory category)
+        {
+            Name = name;
+            Author = author;
+            Category = category;
+
+
+        }
+        public void DisplayCopies()
+        {
+            Console.WriteLine($"Copies: {copies}");
+        }
     }
 }
+
+
+
+
 
--- Libarary/Flyweight/BookFactory.cs
+++ Libarary/Flyweight/BookFactory.cs
@@ -8,7 +8,25 @@ namespace Libarary.Flyweight
 {
     internal class BookFactory
     {
-          
 
+
+        private static Dictionary<string, BookCopy> _books = new Dictionary<string, BookCopy>();
+
+        public BookFactory()
+        {
+
+        }
+        public BookCopy GetBook(string name, string author, BookCategory category)
+        {
+            string key = $"{name}{author}{category}";
+
+            if (!_books.ContainsKey(key))
+            {
+                _books[key] = new BookCopy(name, author, category);
+            }
+
+
+            return _books[key];
+        }
     }
 }
